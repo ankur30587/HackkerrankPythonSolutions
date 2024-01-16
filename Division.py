676f1548a88b08d22788e8477a18d0e744d3bab4
ankur30587 <ankur30587@gmail.com>
2024-01-16 23:53:14 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == "__main__":
+    # Read input from the user
+    a = int(input())
+    b = int(input())
+
+    # Perform integer division and float division
+    integer_division = a // b
+    float_division = a / b
+
+    # Print the results
+    print(integer_division)
+    print(float_division)
