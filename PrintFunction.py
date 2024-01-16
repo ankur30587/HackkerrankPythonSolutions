90e9ebf58fb116097877312a8e04bbc2b385c941
ankur30587 <ankur30587@gmail.com>
2024-01-16 23:59:19 +0530

    Create PrintFunction.py

---
+++ PrintFunction.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    # Read input from the user
+    n = int(input())
+
+    # Print the consecutive integers as a string
+    for i in range(1, n + 1):
+        print(i, end="")
