7c1152ecceac8e5fe5e6376cedf22bf0d8f2f33d
ankur30587 <ankur30587@gmail.com>
2024-01-16 23:51:49 +0530

    Create ArithematicOperators.py

---
+++ ArithematicOperators.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == "__main__":
+    # Read input from the user
+    a = int(input())
+    b = int(input())
+
+    # Calculate and print the sum, difference, and product
+    sum_ab = a + b
+    diff_ab = a - b
+    product_ab = a * b
+
+    print(sum_ab)
+    print(diff_ab)
+    print(product_ab)
